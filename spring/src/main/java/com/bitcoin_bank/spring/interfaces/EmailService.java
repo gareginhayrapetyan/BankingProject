c911fab72ba4b03226a07e96ea7b7cc2db8e68fb
Venera <ven.melkonyan@mail.ru>
2019-02-17 18:20:27 +0400

    spring boot

--- spring/src/main/java/com/bitcoin_bank/spring/interfaces/EmailService.java
+++ spring/src/main/java/com/bitcoin_bank/spring/interfaces/EmailService.java
@@ -1,5 +1,8 @@
 package com.bitcoin_bank.spring.interfaces;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public interface EmailService {
     boolean sendSimpleMessage(String to, String subject, String text);
 
